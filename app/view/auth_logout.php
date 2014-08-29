22b87463451285c1618cdce145f6c42438babaa9
aza <aza@esfriki.com>
2014-08-29 02:06:05 -0300

    logout template

---
+++ app/view/auth_logout.php
@@ -0,0 +1,20 @@
+<div id="main-wrapper">
+<div class="wrapper style1">
+<div class="inner">
+
+<!-- Feature 1 -->
+<section class="container box feature1">
+
+  <div class="row">
+    <div class="12u">
+      <header class="first major">
+      <h2>Chau!</h2>
+      </header>
+    </div>
+  </div>
+</section>
+
+</div>
+</div>
+</div>
+
