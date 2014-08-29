912c6979d1fa8e097d9b55b6b401865120327f3c
aza <aza@esfriki.com>
2014-08-29 01:20:28 -0300

    archivos olvidadods

---
+++ app/view/projects.php
@@ -0,0 +1,55 @@
+<!-- Feature 1 -->
+<section class="container box feature1">
+
+  <div class="row">
+    <div class="12u">
+      <header class="first major">
+        <h2>Check out some of our projects</h2>
+        <p>And what we are doing for <strong>equality and a fair economy</strong></p>
+      </header>
+    </div>
+  </div>
+
+  <div class="row">
+
+    <div class="4u">
+      <section>
+        <a href="#" class="image featured"><img src="/images/pic01.jpg" alt="" /></a>
+        <header class="second icon fa-user">
+          <h3>Here's a Heading</h3>
+          <p>And a subtitle</p>
+        </header>
+
+    </div>
+
+    <div class="4u">
+      <section>
+        <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+        <header class="second icon fa-cog">
+          <h3>Also a Heading</h3>
+          <p>And another subtitle</p>
+        </header>
+      </section>
+    </div>
+
+    <div class="4u">
+      <section>
+        <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+        <header class="second icon fa-bar-chart-o">
+          <h3>Another Heading</h3>
+          <p>And yes, a subtitle</p>
+        </header>
+      </section>
+    </div>
+
+  </div>
+  <div class="row">
+    <div class="12u">
+      <p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus. Praesent semper 
+      bibendum ipsum, et tristique augue fringilla eu. Vivamus id risus vel dolor auctor euismod 
+      quis eget mi. Etiam eu ante risus. Aliquam erat volutpat. Aliquam luctus mattis lectus sit 
+      amet pulvinar. Nam nec turpis.</p>
+    </div>
+  </div>
+</section>
+
