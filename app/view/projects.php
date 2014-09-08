242d098316faaedd9ea6fa494ba27e4d049d9bc1
aza <aza@esfriki.com>
2014-09-08 03:39:37 -0300

    changing text and images

--- app/view/projects.php
+++ app/view/projects.php
@@ -14,20 +14,24 @@
 
     <div class="4u">
       <section>
-        <a href="#" class="image featured"><img src="/images/pic01.jpg" alt="" /></a>
+        <ul>
+          <li>No ads or trackers</li>
+          <li>Forgets you on demand</li>
+          <li>No private data collected</li>
+        </ul>
         <header class="second icon fa-user">
-          <h3>Here's a Heading</h3>
-          <p>And a subtitle</p>
+          <h3>We take care of your privacy</h3>
+          <p>And your friend's</p>
         </header>
 
     </div>
 
     <div class="4u">
       <section>
-        <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+        <a href="#" class="image featured"><img src="images/faircoop.png" alt="Fair Coop" /></a>
         <header class="second icon fa-cog">
-          <h3>Also a Heading</h3>
-          <p>And another subtitle</p>
+          <h3>Fair Coop</h3>
+          <p>The earth cooperative</p>
         </header>
       </section>
     </div>
@@ -36,8 +40,8 @@
       <section>
         <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
         <header class="second icon fa-bar-chart-o">
-          <h3>Another Heading</h3>
-          <p>And yes, a subtitle</p>
+          <h3>Fair Credit</h3>
+          <p>To promote fair projects</p>
         </header>
       </section>
     </div>
